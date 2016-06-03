0365142c6fd4067206be748f4db3727b50e8c157
Alyssa <aransbury@gmail.com>
2016-06-03 12:28:15 -0400

    Add omniauth callbacks controller and omniauth route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {omniauth_callbacks: 'omniauth_callbacks'}
   root 'welcome#index'
-
-  resources :sessions, only: [:new, :create, :destroy]
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
 end
